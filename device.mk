3119e83bdea4aa180cdcc19ae74091fb2f4d6bc7
Danny Baumann <dannybaumann@web.de>
2018-07-18 17:25:32 +0200

    RMX1821: Enable call recording
    Change-Id: I1b4478b330b13cf5d0fe1ed0d40f43e4b3825f33

--- device.mk
+++ device.mk
@@ -18,7 +18,8 @@ PRODUCT_COPY_FILES := \
 	
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-	$(LOCAL_PATH)/overlay
+	$(LOCAL_PATH)/overlay \
+	$(LOCAL_PATH)/overlay-lineage
 
 # Dependency of kpoc_charger
 PRODUCT_PACKAGES += \
