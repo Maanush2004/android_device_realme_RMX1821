9d2ce314f87121c94eff3eaa6eca2384bfa4f95b
Maanush Putcha <p.maanush@gmail.com>
2021-02-28 21:02:06 +0530

    RMX1821: Build EngineerMode from OSS
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- device.mk
+++ device.mk
@@ -96,5 +96,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # IMS
 $(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
 
+# EngineerMode
+$(call inherit-product, vendor/mediatek/ims/mtk-engi.mk)
+
 # Call proprietary blob setup
 $(call inherit-product-if-exists, vendor/realme/RMX1821/RMX1821-vendor.mk)
