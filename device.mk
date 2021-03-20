4930d0a31efe5f58504ce9b29ce5dcd66bfdbe9d
SamarV-121 <samarvispute121@gmail.com>
2021-03-20 10:25:12 +0530

    RMX1821: Compile RealmeParts
     * https://github.com/HyperTeam/packages_apps_RealmeParts
    
    Change-Id: I2ec5865961d683f5bb0a3db8d526063bf7b7fe7b
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -111,5 +111,8 @@ $(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
 # EngineerMode
 $(call inherit-product, vendor/mediatek/ims/mtk-engi.mk)
 
+# Parts
+$(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
+
 # Call proprietary blob setup
 $(call inherit-product-if-exists, vendor/realme/RMX1821/RMX1821-vendor.mk)
