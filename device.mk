53f3ecadc4e7c9f24e84ead5a1a3826ed51a362f
SamarV-121 <samarvispute121@pm.me>
2021-09-03 14:45:57 +0200

    RMX1821: Add a shim for vtservice
     - F linker  : CANNOT LINK EXECUTABLE "/system/bin/vtservice": cannot locate symbol "_ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi" referenced by "/system/lib/libsink.so"

--- device.mk
+++ device.mk
@@ -106,6 +106,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # IMS
 $(call inherit-product, vendor/realme-ims/realme-ims-vendor.mk)
 
+PRODUCT_PACKAGES += \
+    libshim_vtservice
+
 # Parts
 $(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
 
