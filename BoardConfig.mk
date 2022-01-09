3456529d7dd744baba4e699840333c2ef1d2d138
Maanush Putcha <p.maanush@gmail.com>
2022-01-09 21:22:07 +0530

    RMX1821: Revert "Provide a shim for libsink"
    This reverts commit 772bec1f6c5608764300c268389ff2847a99c02d.
    
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,9 +108,7 @@ TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE := true
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 # Symbols
-TARGET_LD_SHIM_LIBS += \
-        /system/lib/libshowlogo.so|libshim_showlogo.so \
-        /system/lib/libsink.so|libshim_vtservice.so
+TARGET_LD_SHIM_LIBS := /system/lib/libshowlogo.so|libshim_showlogo.so
 
 # Treble
 BOARD_VNDK_VERSION := current
