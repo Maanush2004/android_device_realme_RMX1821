41e67675392c2d98a5b95317b4a7102177c6c51d
SamarV-121 <samarvispute121@gmail.com>
2022-04-09 18:26:03 +0530

    RMX1821: Update performance profiles
    * getting handled in realmeparts - HyperTeam/android_packages_apps_RealmeParts@46d021a
    
    Change-Id: I8546af97b68632990f6d02dc07d454edb656342b
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- rootdir/bin/perf_profile.sh
+++ rootdir/bin/perf_profile.sh
@@ -1,63 +1,187 @@
 #!/system/bin/sh
 
+## Common-default settings
+
+# GED modules
+echo 0 >/sys/module/ged/parameters/gx_game_mode
+echo 0 >/sys/module/ged/parameters/gx_force_cpu_boost
+echo 0 > /sys/module/ged/parameters/boost_amp
+echo 0 > /sys/module/ged/parameters/boost_extra
+echo 0 > /sys/module/ged/parameters/boost_gpu_enable
+echo 0 > /sys/module/ged/parameters/enable_cpu_boost
+echo 0 > /sys/module/ged/parameters/enable_gpu_boost
+echo 0 > /sys/module/ged/parameters/enable_game_self_frc_detect
+echo 100 > /sys/module/ged/parameters/gpu_idle
+
+# Disable all PPM
+echo 0 > /proc/ppm/enabled
+
+# Enable Thermal Throttle
+echo 4 1 > /proc/ppm/policy_status
+
+# cpufreq mode
+echo 0 > /proc/cpufreq/cpufreq_cci_mode
+echo 0 > /proc/cpufreq/cpufreq_power_mode
+echo 0 > /sys/devices/system/cpu/perf/enable
+
+# Default CPU Freq
+#big cluster
+echo 0 -1 > /proc/ppm/policy/hard_userlimit_max_cpu_freq
+echo 0 -1 > /proc/ppm/policy/hard_userlimit_min_cpu_freq
+#LITTLE cluster
+echo 1 -1 > /proc/ppm/policy/hard_userlimit_max_cpu_freq
+echo 1 -1 > /proc/ppm/policy/hard_userlimit_min_cpu_freq
+
+# Default GPU Freq
+echo 0 > /proc/gpufreq/gpufreq_opp_freq
+
+# set sched to hybrid (HMP,EAS)
+echo 2 > /sys/devices/system/cpu/eas/enable
+
+# schedutil rate-limit
+echo 1000 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
+echo 1000 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
+
+# cpusets
+echo 0-7 > /dev/cpuset/foreground/cpus
+echo 0-5 > /dev/cpuset/background/cpus
+echo 0-7 > /dev/cpuset/system-background/cpus
+echo 0-7 > /dev/cpuset/top-app/cpus
+echo 0-7 > /dev/cpuset/restricted/cpus
+
+# OPPO touchpanel
+echo 0 > /proc/touchpanel/oppo_tp_limit_enable
+echo 1 > /proc/touchpanel/oppo_tp_direction
+echo 0 > /proc/touchpanel/game_switch_enable
+
 case $1 in
+0)
+	## Balanced profile(use all default settings)
+	;;
 1)
-	# GED modules
+	## Power saving profile
+
+    # GED Modules
+    echo 0 >/sys/module/ged/parameters/gx_game_mode
+	echo 0 >/sys/module/ged/parameters/gx_force_cpu_boost
+	echo 0 > /sys/module/ged/parameters/boost_amp
+	echo 0 > /sys/module/ged/parameters/boost_extra
+	echo 0 > /sys/module/ged/parameters/boost_gpu_enable
+	echo 0 > /sys/module/ged/parameters/enable_cpu_boost
+	echo 0 > /sys/module/ged/parameters/enable_gpu_boost
+	echo 0 > /sys/module/ged/parameters/enable_game_self_frc_detect
+	echo 100 > /sys/module/ged/parameters/gpu_idle
+	
+	# cpufreq mode
+	echo 1 >/proc/cpufreq/cpufreq_power_mode
+	echo 0 >/proc/cpufreq/cpufreq_cci_mode
+	echo 0 > /sys/devices/system/cpu/perf/enable
+
+    # force sched to EAS
+    echo 1 > /sys/devices/system/cpu/eas/enable
+
+    # sched rate
+	echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
+	echo 1000 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
+	
+	# CPU SchedTune
+	echo 0-5 > /dev/cpuset/foreground/cpus
+	echo 0-3 > /dev/cpuset/background/cpus
+	echo 0-7 > /dev/cpuset/system-background/cpus
+	echo 0-7 > /dev/cpuset/top-app/cpus
+	echo 0-4 > /dev/cpuset/restricted/cpus
+	
+	# PPM
+	echo 1 > /proc/ppm/enabled
+	echo 0 1 > /proc/ppm/policy_status
+	echo 1 1 > /proc/ppm/policy_status
+	echo 2 1 > /proc/ppm/policy_status
+	echo 3 1 > /proc/ppm/policy_status
+	echo 5 0 > /proc/ppm/policy_status
+	echo 6 1 > /proc/ppm/policy_status
+	echo 7 1 > /proc/ppm/policy_status
+	echo 8 1 > /proc/ppm/policy_status
+	echo 9 0 > /proc/ppm/policy_status
+	#enable thermal throttle
+    echo 4 1 > /proc/ppm/policy_status
+
+    # Set CPU Freq
+    # big cluster
+	echo 1 1014000 > /proc/ppm/policy/hard_userlimit_max_cpu_freq
+	echo 1 793000 > /proc/ppm/policy/hard_userlimit_min_cpu_freq
+	# LITTLE cluster
+	echo 0 1014000 > /proc/ppm/policy/hard_userlimit_max_cpu_freq
+	echo 0 793000 > /proc/ppm/policy/hard_userlimit_min_cpu_freq
+	
+	# Lock Lowest GPU Freq
+    echo 300000 > /proc/gpufreq/gpufreq_opp_freq
+	
+	# OPPO touchpanel
+	echo 0 > /proc/touchpanel/oppo_tp_limit_enable
+    echo 1 > /proc/touchpanel/oppo_tp_direction
+    echo 0 > /proc/touchpanel/game_switch_enable
+	;;
+2)
+	## Performance Profile
+
+    # GED Modules
 	echo 1 >/sys/module/ged/parameters/gx_game_mode
 	echo 1 >/sys/module/ged/parameters/gx_force_cpu_boost
+	echo 1 > /sys/module/ged/parameters/boost_amp
+	echo 1 > /sys/module/ged/parameters/boost_extra
+	echo 1 > /sys/module/ged/parameters/boost_gpu_enable
+	echo 1 > /sys/module/ged/parameters/enable_cpu_boost
+	echo 1 > /sys/module/ged/parameters/enable_gpu_boost
+	echo 1 > /sys/module/ged/parameters/enable_game_self_frc_detect
+	echo 0 > /sys/module/ged/parameters/gpu_idle
 
-	# cpufreq modes
-	echo 3 >/proc/cpufreq/cpufreq_power_mode
-	echo 1 >/proc/cpufreq/cpufreq_cci_mode
+    # cpufreq mode
+	echo 3 > /proc/cpufreq/cpufreq_power_mode
+	echo 1 > /proc/cpufreq/cpufreq_cci_mode
+    echo 1 > /sys/devices/system/cpu/perf/enable
 
-	# Force sched to EAS
-	echo 1 > /sys/devices/system/cpu/eas/enable
+	# force sched to HMP
+	echo 0 > /sys/devices/system/cpu/eas/enable
 
-	echo 50000 >/sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
-	echo 0 >/sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
+    # sched rate
+	echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
+	echo 0 > /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
 
-	# Cpusets
-	echo 0 >/dev/cpuset/background/cpus
-	echo 0-3 >/dev/cpuset/system-background/cpus
-	echo 0-3 >/dev/cpuset/restricted/cpus
+    # CPU SchedTune
+	echo 0-7 > /dev/cpuset/foreground/cpus
+	echo 0 > /dev/cpuset/background/cpus
+	echo 0-7 > /dev/cpuset/system-background/cpus
+	echo 0-7 > /dev/cpuset/top-app/cpus
+	echo 0 > /dev/cpuset/restricted/cpus
 
-	# Lock gpu frequency
-	echo 800000 > /proc/gpufreq/gpufreq_opp_freq
-
-	# Disable ppm
-	echo 0 > /proc/ppm/enabled
-
-	## Limit minimum cpu frequency
-	# big cluster
-	# echo 1 1796000 >/proc/ppm/policy/hard_userlimit_min_cpu_freq
-
-	# LITTLE cluster
-	# echo 0 1866000 >/proc/ppm/policy/hard_userlimit_min_cpu_freq
-	;;
-0)
-	## Restore defaults
-
-	echo 0 >/sys/module/ged/parameters/gx_game_mode
-	echo 0 >/sys/module/ged/parameters/gx_force_cpu_boost
-
-	echo 0 >/proc/cpufreq/cpufreq_power_mode
-	echo 0 >/proc/cpufreq/cpufreq_cci_mode
-
-	# Set sched to hybrid (HMP,EAS)
-	echo 2 > /sys/devices/system/cpu/eas/enable
-
-	echo 10000 >/sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us
-	echo 10000 >/sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us
-
-	echo 0-3 >/dev/cpuset/background/cpus
-	echo 0-5 >/dev/cpuset/system-background/cpus
-	echo 0-7 >/dev/cpuset/restricted/cpus
-
-	echo 0 > /proc/gpufreq/gpufreq_opp_freq
-
+    # PPM
 	echo 1 > /proc/ppm/enabled
+	echo 0 1 > /proc/ppm/policy_status
+	echo 1 1 > /proc/ppm/policy_status
+	echo 2 1 > /proc/ppm/policy_status
+	echo 3 1 > /proc/ppm/policy_status
+	echo 5 1 > /proc/ppm/policy_status
+	echo 6 1 > /proc/ppm/policy_status
+	echo 7 1 > /proc/ppm/policy_status
+	echo 8 1 > /proc/ppm/policy_status
+	echo 9 1 > /proc/ppm/policy_status
+	#disable thermal throttle
+    echo 4 0 > /proc/ppm/policy_status
 
-	# echo 1 -1 >/proc/ppm/policy/hard_userlimit_min_cpu_freq
-	# echo 0 -1 >/proc/ppm/policy/hard_userlimit_min_cpu_freq
+    # Set maximum CPU Freq
+    # big cluster
+	echo 1 1989000 >/proc/ppm/policy/hard_userlimit_max_cpu_freq
+	echo 1 1989000 >/proc/ppm/policy/hard_userlimit_min_cpu_freq
+    # LITTLE cluster
+	echo 0 1989000 >/proc/ppm/policy/hard_userlimit_max_cpu_freq
+	echo 0 1989000 >/proc/ppm/policy/hard_userlimit_min_cpu_freq
+	
+    # Set maximum GPU Freq
+	echo 900000 > /proc/gpufreq/gpufreq_opp_freq
+
+    # OPPO touchpanel
+	echo 0 > /proc/touchpanel/oppo_tp_limit_enable
+	echo 1 > /proc/touchpanel/oppo_tp_direction
+	echo 1 > /proc/touchpanel/game_switch_enable
 	;;
 esac
